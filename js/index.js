a8b4e237188b25e4e3d6dcd2eec6a04528a50d3f
Subigya Nepal <subigya_0324@deerwalk.edu.np>
2015-03-14 21:05:38 +0545

    splash

--- js/index.js
+++ js/index.js
@@ -70,9 +70,9 @@ var app = {
   // The scope of 'this' is the event. In order to call the 'receivedEvent'
   // function, we must explicity call 'app.receivedEvent(...);'
   onDeviceReady: function() {
-      navigator.splashscreen.show();
-    app.receivedEvent('deviceready');
-    
+         app.receivedEvent('deviceready');
+     navigator.splashscreen.show();
+
      // if (!window.plugins.socialsharing) {alert('sorry not initialized');}
   },
   // Update DOM on a Received Event
